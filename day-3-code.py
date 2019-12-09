d0f83212b18722006b3274a1c4288d6a5a5208a7
Alex C. Viana <alexcostaviana@gmail.com>
2019-12-08 23:12:55 -0600

    Completed day 3 part 2.

--- day-3-code.py
+++ day-3-code.py
@@ -9,6 +9,12 @@ class Wire:
         self.instructions = instructions.split(',')
         self.wire_route = [(0,0)]
 
+    def distance_to_point(self, point):
+        for step in range(len(self.wire_route)):
+            if self.wire_route[step] == point:
+                return step
+        return None
+
     def next_position(self, instruction):
         direction = instruction[0]
         magnitude = int(instruction[1:])
@@ -49,12 +55,18 @@ def find_overlap(wire1, wire2):
     return [item for item in overlap if item != (0, 0)]
 
 
-def test_wire_class():
+def test_wire_route():
     wire = Wire('L1,R1,U1,D1')
     assert wire.route() == [(0, 0), (-1, 0), (0, 0), (0, 1), (0, 0)]
 
     wire = Wire('U2,R2')
     assert wire.route() == [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
+
+
+def test_wire_steps():
+    wire = Wire('U2,R2')
+    wire.route()
+    assert wire.distance_to_point((2, 2)) == 4
 
 
 def test_find_overlap():
@@ -67,22 +79,52 @@ def test_find_overlap():
     assert [(2, 2)] == find_overlap(wire1, wire2)
 
 
-def test_main():
+def test_find_closest_point():
     assert (
-        main(
+        find_closest_point(
             'R75,D30,R83,U83,L12,D49,R71,U7,L72',
             'U62,R66,U55,R34,D71,R55,D58,R83'
         ) == 159
     )
     assert (
-        main(
+        find_closest_point(
             'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
             'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
         ) == 135
     )
 
 
-def main(instructions1, instructions2):
+def test_find_number_of_steps():
+    assert (
+        find_closest_point(
+            'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+            'U62,R66,U55,R34,D71,R55,D58,R83'
+        ) == 159
+    )
+    assert (
+        find_closest_point(
+            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+        ) == 135
+    )
+
+
+def test_find_number_of_steps():
+    assert (
+        find_number_of_steps(
+            'R75,D30,R83,U83,L12,D49,R71,U7,L72',
+            'U62,R66,U55,R34,D71,R55,D58,R83'
+        ) == 610
+    )
+    assert (
+        find_number_of_steps(
+            'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
+            'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'
+        ) == 410
+    )
+
+
+def find_closest_point(instructions1, instructions2):
     wire1 = Wire(instructions1)
     wire2 = Wire(instructions2)
     overlap_list = find_overlap(wire1, wire2)
@@ -90,7 +132,23 @@ def main(instructions1, instructions2):
     return min(overlap_distances)
 
 
+def find_number_of_steps(instructions1, instructions2):
+    wire1 = Wire(instructions1)
+    wire2 = Wire(instructions2)
+    overlap_list = find_overlap(wire1, wire2)
+    step_list = [
+        wire1.distance_to_point(item) + wire2.distance_to_point(item)
+        for item in overlap_list
+    ]
+    return min(step_list)
+
+
 if __name__ == '__main__':
     with open('day-3-input.txt', 'r') as f:
         data = f.readlines()
-    print(f'Distance is {main(data[0].strip(), data[1].strip())}')
+    print(
+        f'Distance is {find_closest_point(data[0].strip(), data[1].strip())}'
+    )
+    print(
+        f'Shortest route is {find_number_of_steps(data[0].strip(), data[1].strip())}'
+    )
