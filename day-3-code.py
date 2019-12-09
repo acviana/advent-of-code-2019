7490b9693e7a09b615afe57092319cda39021235
Alex C. Viana <alexcostaviana@gmail.com>
2019-12-08 22:53:48 -0600

    Working solution for day 3.

--- day-3-code.py
+++ day-3-code.py
@@ -7,68 +7,64 @@ class Wire:
 
     def __init__(self, instructions):
         self.instructions = instructions.split(',')
-        self.current_position = (0, 0)
-        self.wire_route = []
+        self.wire_route = [(0,0)]
 
     def next_position(self, instruction):
         direction = instruction[0]
         magnitude = int(instruction[1:])
         if direction == 'L':
-            self.current_position = (
-                self.current_position[0] - magnitude,
-                self.current_position[1]
-            )
+            for step in range(magnitude):
+                self.wire_route += [(
+                    self.wire_route[-1][0] - 1,
+                    self.wire_route[-1][1]
+                )]
         elif direction == 'R':
-            self.current_position = (
-                self.current_position[0] + magnitude,
-                self.current_position[1]
-            )
+            for step in range(magnitude):
+                self.wire_route += [(
+                    self.wire_route[-1][0] + 1,
+                    self.wire_route[-1][1]
+            )]
         elif direction == 'U':
-            self.current_position = (
-                self.current_position[0],
-                self.current_position[1] + magnitude
-            )
+            for step in range(magnitude):
+                self.wire_route += [(
+                    self.wire_route[-1][0],
+                    self.wire_route[-1][1] + 1
+            )]
         elif direction == 'D':
-            self.current_position = (
-                self.current_position[0],
-                self.current_position[1] - magnitude
-            )
+            for step in range(magnitude):
+                self.wire_route += [(
+                    self.wire_route[-1][0],
+                    self.wire_route[-1][1] - 1
+            )]
         else:
-            raise KeyError(f'Unexpected instricution direction {instruction}')
-        return self.current_position
+            raise KeyError(f'Unexpected instruction direction {instruction}')
 
     def route(self):
-        return [
-            self.next_position(instruction) for instruction in self.instructions
-        ]
+        [self.next_position(instruction) for instruction in self.instructions]
+        return self.wire_route
 
 
 def find_overlap(wire1, wire2):
-    wire1_path = wire1.route()
-    wire2_path = wire2.route()
-    overlap = set(wire1_path).intersection(wire2_path)
-    return [item for item in overlap]
+    overlap = set(wire1.route()).intersection(wire2.route())
+    return [item for item in overlap if item != (0, 0)]
 
 
 def test_wire_class():
-    wire = Wire('R75,D30,R83,U83,L12,D49,R71,U7,L72')
-    assert wire.instructions == ['R75', 'D30', 'R83', 'U83', 'L12', 'D49', 'R71', 'U7', 'L72']
-    # assert wire.route() == [(75, 0), (75, -30), (158, -30)]
-    assert isinstance(wire.instructions, list)
-
     wire = Wire('L1,R1,U1,D1')
-    wire_route = wire.route()
-    assert wire_route == [(-1, 0), (0, 0), (0, 1), (0, 0)]
+    assert wire.route() == [(0, 0), (-1, 0), (0, 0), (0, 1), (0, 0)]
+
+    wire = Wire('U2,R2')
+    assert wire.route() == [(0, 0), (0, 1), (0, 2), (1, 2), (2, 2)]
 
 
 def test_find_overlap():
     wire1 = Wire('U1,D1')
     wire2 = Wire('D1,U1')
-    assert (0, 0) in find_overlap(wire1, wire2)
+    assert [] == find_overlap(wire1, wire2)
 
-    wire1 = Wire('R75,D30,R83,U83,L12,D49,R71,U7,L72')
-    wire2 = Wire('U62,R66,U55,R34,D71,R55,D58,R83')
-    assert len(find_overlap(wire1, wire2)) == 2
+    wire1 = Wire('U2,R2')
+    wire2 = Wire('R2,U2')
+    assert [(2, 2)] == find_overlap(wire1, wire2)
 
 
 def test_main():
@@ -89,13 +85,12 @@ def test_main():
 def main(instructions1, instructions2):
     wire1 = Wire(instructions1)
     wire2 = Wire(instructions2)
-    overlap = find_overlap(wire1, wire2)
-    return (
-        abs(overlap[0][0] - overlap[1][0]) +
-        abs(overlap[0][1] - overlap[1][1])
-    )
+    overlap_list = find_overlap(wire1, wire2)
+    overlap_distances = [abs(item[0]) + abs(item[1]) for item in overlap_list]
+    return min(overlap_distances)
 
 
 if __name__ == '__main__':
-    wire = Wire('R75,D30,R83,U83,L12,D49,R71,U7,L72')
-    print(wire.instructions)
+    with open('day-3-input.txt', 'r') as f:
+        data = f.readlines()
+    print(f'Distance is {main(data[0].strip(), data[1].strip())}')
